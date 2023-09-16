52bc2ed68292ed05256cb7ad35c4ffe5af2b657c
isaquic <isaqui.pessoal@gmail.com>
2023-09-16 12:00:18 -0300

    Commit unico

--- atividade.js
+++ atividade.js
@@ -1,25 +1,58 @@
 import entradaDados from 'readline-sync';
 
-console.log("\n Aplicação de Juros: \n");
+console.log("\n Escolha uma das alternativas: \n");
+console.log(" 1 - Listar os salários mínimos de 2010 à 2020 \n");
+console.log(" 2 - Listar o índice IPCA de 2010 à 2020 \n");
+console.log(" 3 - Comparação entre o percentual de aumento salarial e o IPCA \n");
 
-let valor = entradaDados.question("Informe o valor devido: R$ ");
-let dias = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ")
+let dados = [
+    { ano: 2010, salario: 510, inflacao: 5.91 },
+    { ano: 2011, salario: 545, inflacao: 6.50 },
+    { ano: 2012, salario: 622, inflacao: 5.84 },
+    { ano: 2013, salario: 678, inflacao: 5.91 },
+    { ano: 2014, salario: 724, inflacao: 6.41 },
+    { ano: 2015, salario: 788, inflacao: 10.67 },
+    { ano: 2016, salario: 880, inflacao: 6.29 },
+    { ano: 2017, salario: 937, inflacao: 2.95 },
+    { ano: 2018, salario: 954, inflacao: 3.75 },
+    { ano: 2019, salario: 998, inflacao: 4.31 },
+    { ano: 2020, salario: 1045, inflacao: 4.52 },
+];
 
-let resultado = ''
-
-//valor += (dias * 10) / 365; // Calculo dos juros
-
-console.log("\n Valor original da dívida: R$ "+valor+"\n")
-console.log("\n Dias de atraso: "+dias+"\n")
-console.log("\n Taxa de Juros até 15 dias é de: 5% \n")
-console.log("\n Taxa de Juros acima de 15 dias é de: 10% \n")
+let escolha = entradaDados.question("\n Digite sua escolha: \n");
 
 
-if (dias > 15 && dias < 31){
-    resultado = Number(valor) + (valor * 0.10)
-}else {
-    resultado = Number(valor) + (valor * 0.5)
+
+switch (escolha) {
+    case "1":
+        for (let dados_salario of dados) {
+            let ano = dados_salario.ano;
+            let valor = dados_salario.salario.toFixed(2).replace(".", ",");
+            console.log(`O ano ${ano} teve um salário mínimo igual a R$ ${valor}`);
+        }
+        break;
+    case "2":
+        for (let dados_ipc of dados) {
+            let ano = dados_ipc.ano;
+            let dados_inflacao = dados_ipc.inflacao.toFixed(2);
+            console.log(`O ano ${ano} teve um IPC foi igual a ${dados_inflacao}%`);
+        }
+        break;
+    case "3":
+        for (let i = 1; i < dados.length; i++) {
+           
+            
+            let salarioAnterior = dados[i - 1].salario;
+            let salarioAno = dados[i].salario;
+            let diferenca = salarioAno - salarioAnterior;
+            let percentualCrescimento = (diferenca / salarioAnterior) * 100;
+            let crescimentoFormatado = percentualCrescimento.toFixed(2).replace(".", ",");
+            
+            console.log(`${crescimentoFormatado}% de crescimento no ano ${dados[i].ano}`);
+                
+            
+        }
+        break;
+    default:
+        console.log("Opção inválida");
 }
-
-console.log("\n Valor total com juros é de: "+resultado)
-
